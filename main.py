a752314bb333dbadde97f32fd4264ee63839a740
Angelina Bidna <anngktw@gmail.com>
2026-09-09 10:38:49 +0300

    Add Python calculator

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print("Hello, world!")
+first_number = float(input("Enter the first number: "))
+operation = input("Choose an operation (+, -, *, /): ")
+second_number = float(input("Enter the second number: "))
+
+if operation == "+":
+    result = first_number + second_number
+    print("Result:", result)
+elif operation == "-":
+    result = first_number - second_number
+    print("Result:", result)
+elif operation == "*":
+    result = first_number * second_number
+    print("Result:", result)
+elif operation == "/":
+    if second_number == 0:
+        print("Error: division by zero is not allowed.")
+    else:
+        result = first_number / second_number
+        print("Result:", result)
+else:
+    print("Error: choose one of these operations: +, -, *, /.")
